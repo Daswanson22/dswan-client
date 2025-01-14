f005f12577c6218b4862696dcc709187acf8e111
Dylan Swanson <81135334+Daswanson22@users.noreply.github.com>
2025-01-13 16:46:22 -0800

    skills page updated

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,23 +1,44 @@
 import React from 'react';
 
+const skillData = [
+  {
+    title: 'Project Management Skills',
+    skills: ['Project Coordination', 'Task Prioritization', 'Risk Management', 'Client Communication', 'Team Leadership', 'Time Management', 'Documentation Management']
+  },
+  {
+    title: 'Languages',
+    skills: ['JavaScript', 'Python', 'Java', 'C#', 'C++']
+  },
+  {
+    title: 'Frameworks/Technologies',
+    skills: ['React', 'Tailwind', 'JQuery', 'Express.js', 'Git', 'NGINX']
+  },
+  {
+    title: 'Database\'s',
+    skills: ['Google Firebase', 'SQL', 'MongoDB']
+  },
+  {
+    title: 'Tools',
+    skills: ['AWS (EC2, S3, Cloudfront, IAM)', 'GCP (Compute Engine, VPC, IAM)', 'Postman', 'Unity']
+  }
+]
+
 const Skills = () => {
   return (
     <div className="space-y-4 flex flex-col items-start justify-center text-white bg-opacity-25 rounded-md">
-      <h2 className="text-4xl text-center bg-light-black w-full py-2 font-medium text-fun-teal border-solid border-x-8 border-fun-teal">Skills</h2>
+      <h2 className="text-4xl text-center bg-light-black py-2 px-8 border-solid border-b-2 border-fun-teal font-medium text-fun-teal">Skills</h2>
 
-      <div className='bg-light-black flex justify-center items-center'>
-        <div className="py-4 px-8 rounded-md border-solid border border-white w-fit">
-          <h3 className="text-xl font-semibold text-fun-teal italic">Project Management Skills</h3>
-          <p className="text-lg font-light">Project Coordination, Task Prioritization, Risk Management, Client Communication, Team Leadership, Time Management, Documentation Management</p>
-          <h3 className="text-xl font-semibold text-fun-teal italic">Languages</h3>
-          <p className="text-lg font-light">JavaScript, Python, Java, C#, C++</p>
-          <h3 className="text-xl font-semibold mt-4 text-fun-teal italic">Frameworks/Technologies</h3>
-          <p className="text-lg font-light">React, Tailwind, JQuery, Express.js, Git, NGINX</p>
-          <h3 className="text-xl font-semibold mt-4 text-fun-teal italic">Database's</h3>
-          <p className="text-lg font-light">Google Firebase, SQL, MongoDB</p>
-          <h3 className="text-xl font-semibold mt-4 text-fun-teal italic">Tools</h3>
-          <p className="text-lg font-light">AWS (EC2, S3, Cloudfront, IAM), Firebase, MongoDB,  Postman, Unity</p>
-        </div>
+      <div className='flex justify-between items-start flex-wrap'>
+        {skillData.map((skill, index) => (
+          <div key={index} className="w-[300px] h-[300px] border p-2">
+            <h3 className="text-2xl text-fun-teal">{skill.title}</h3>
+            <ul className="list-disc list-inside">
+              {skill.skills.map((item, index) => (
+                <li key={index} className="text-lg">{item}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
       </div>
     </div>
   );
