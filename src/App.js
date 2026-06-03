6d7b555b34c3b3fc306896cbbc0746676e169205
Dylan Swanson <daswanson22@gmail.com>
2026-06-02 18:38:07 -0700

    remove animationprovider

--- src/App.js
+++ src/App.js
@@ -2,17 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Home from './components/Home';
 import Layout from './components/Layout';
-import AnimationProvider from './contexts/AnimationProvider';
 import "./index.css";
 
 function App() {
   return (
     <Router>
-      <AnimationProvider>
-        <Layout>
-          <Home />
-        </Layout>
-      </AnimationProvider>
+      <Layout>
+        <Home />
+      </Layout>
     </Router>
   );
 }
